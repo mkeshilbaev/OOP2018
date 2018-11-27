f42aa1d90578f2fe1a8b3673bead2838cd1b7757
mkeshilbaev <m.keshilbaev@gmail.com>
2018-11-27 22:46:00 +0600

    Team project

---
+++ IntranetOOP/src/Degree.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+
+public enum Degree implements Serializable
+{
+    TUTOR, LECTOR, SENIOR_LECTOR, PROFESSOR;
+}
---
+++ IntranetOOP/src/Employee.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+public class Employee extends User implements Serializable
+{
+    protected double salary;
+
+    public Employee(){}
+
+    public Employee(String name, String surname, String login, Type.UserType type, double salary)
+    {
+        super(name,surname,login,type);
+        this.salary = salary;
+    }
+}
